6926de6d13a9309888c97e6eec62f1eb19589910
冯宇 <yu.feng@kinglandiot.com>
2014-06-29 11:56:04 +0800

    优化算法，章节名不会只显示一半了，不再需要lmxl库1

--- getComic.py
+++ getComic.py
@@ -4,33 +4,39 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 import requests
-from lxml import html
 import base64
 import re
 import json
 import os
 
-#url = 'http://ac.qq.com/Comic/comicInfo/id/511915'
-#url = 'http://ac.qq.com/Comic/ComicInfo/id/17114'
-url = 'http://ac.qq.com/Comic/comicInfo/id/518333'   #要爬取的漫画首页
-#path = 'C:\\Users\\FJP\\Desktop'
-path = '/home/fengyu'  #下载图片存放路径
-if not os.path.isdir(path):
-    os.mkdir(path)
+
 requestSession = requests.session()
 
-def getContent(url):
-    homePage = requestSession.get(url)
-    tree = html.fromstring(homePage.text)
-    contentURL = tree.xpath('/html/body/div/div/ol/li/p/span/a/@href')
-    contentURL = [ 'http://ac.qq.com' + url for url in contentURL ]
-    comicName = tree.xpath('/html/body/div[3]/div[1]/div[1]/div[2]/div[1]/div[1]/h2/strong/text()')
-    contentName = tree.xpath('/html/body/div/div/ol/li/p/span/a/text()')
-    return (comicName[0],contentName,contentURL)
-
-def getImgList(url):
+def getId(url):
     numRE = re.compile(r'\d+')
-    id, cid = numRE.findall(url)
+    id = numRE.findall(url)[0]
+    return id    
+
+def getContent(id):
+    getComicInfoUrl = 'http://m.ac.qq.com/GetData/getComicInfo?id={}'.format(id)
+    getComicInfo = requestSession.get(getComicInfoUrl)
+    comicInfoJson = getComicInfo.text
+    comicInfo = json.loads(comicInfoJson)
+    comicName = comicInfo['title']
+    getChapterListUrl = 'http://m.ac.qq.com/GetData/getChapterList?id={}'.format(id)
+    getChapterList = requestSession.get(getChapterListUrl)
+    contentJson = json.loads(getChapterList.text)
+    count = contentJson['length']
+    sortedContentList = []
+    for i in range(count + 1):
+        for item in contentJson:
+            if isinstance(contentJson[item], dict) and contentJson[item]['seq'] == i:
+                sortedContentList.append({item: contentJson[item]})
+                break
+    return (comicName, count, sortedContentList)
+
+def getImgList(contentJson, id):
+    cid = contentJson.keys()[0]
     getPicHashURL = 'http://m.ac.qq.com/View/mGetPicHash?id={}&cid={}'.format(id, cid)
     picJsonPage = requestSession.get(getPicHashURL).text
     picJson = json.loads(picJsonPage)
@@ -60,7 +66,7 @@ def downloadImg(imgUrlList, contentPath):
     print('该集漫画共计{}张图片'.format(count))
     i = 1
     for imgUrl in imgUrlList:
-        print('正在下载第{}张图片...'.format(i), end='')
+        print('\r正在下载第{}张图片...'.format(i), end='')
         imgPath = os.path.join(contentPath, '{0:0>3}.jpg'.format(i))
         downloadRequest = requestSession.get(imgUrl, stream=True)
         with open(imgPath, 'wb') as f:
@@ -69,25 +75,38 @@ def downloadImg(imgUrlList, contentPath):
                     f.write(chunk)
                     f.flush()
         i += 1
-        print('完毕!')
-    print('\n')
+    print('完毕!\n')
 
 def main():
-    comicName,contentName,contentURL = getContent(url)
-    print('漫画名: ' + comicName)
+    #url = 'http://ac.qq.com/Comic/comicInfo/id/511915'
+    url = 'http://ac.qq.com/Comic/ComicInfo/id/17114'
+    #url = 'http://m.ac.qq.com/Comic/comicInfo/id/511915'
+    #url = 'http://ac.qq.com/Comic/comicInfo/id/518333'   #要爬取的漫画首页
+    path = 'C:\\Users\\FJP\\Desktop'
+    #path = '/home/fengyu'  #下载图片存放路径
+    if not os.path.isdir(path):
+       os.mkdir(path)
+    id = getId(url)
+    comicName,count,contentList = getContent(id)
+    contentNameList = []
+    for item in contentList:
+        for k in item:
+            contentNameList.append(item[k]['t'])
+    print('漫画名: {}'.format(comicName))
+    print('章节数: {}'.format(count))
+    print('章节列表:')
+    print('\n'.join(contentNameList))
     comicPath = os.path.join(path, comicName)
     if not os.path.isdir(comicPath):
         os.mkdir(comicPath)
-    print('章节列表: ')
-    print('\n'.join(contentName))
-    i = 0
     print()
-    for content in contentName:
-        print('正在下载章节: {} ...'.format(content))
-        contentPath = os.path.join(comicPath, '{0:0>4}{1}'.format(i, content))
+    i = 0
+    for content in contentList:
+        print('正在下载第{0:0>4}话: {1}'.format(i + 1, contentNameList[i]))
+        contentPath = os.path.join(comicPath, '第{0:0>4}话-{1}'.format(i + 1, contentNameList[i]))
         if not os.path.isdir(contentPath):
             os.mkdir(contentPath)
-        imgList = getImgList(contentURL[i])
+        imgList = getImgList(content, id)
         downloadImg(imgList, contentPath)
         i += 1
     
