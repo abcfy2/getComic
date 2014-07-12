640c3fa6de99c9c260c5cc40aa4c071e03a4f4df
FengYu <yu.feng@kinglandiot.com>
2014-07-12 10:47:58 +0800

    精简部分引用

--- getComic-gui.py
+++ getComic-gui.py
@@ -46,11 +46,9 @@ class TencentComicDownloader(QWidget):
 if __name__ == '__main__':
     import sys
 
-    from PyQt5.QtWidgets import QApplication
-
     app = QApplication(sys.argv)
 
     main = TencentComicDownloader()
     main.show()
 
-    sys.exit(app.exec_())
+    app.exec_()
