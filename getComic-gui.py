501771204b48eb07cb1949966383676ba9acc99b
FengYu <yu.feng@kinglandiot.com>
2014-07-12 10:30:17 +0800

    修改窗口出现位置

--- getComic-gui.py
+++ getComic-gui.py
@@ -30,7 +30,7 @@ class TencentComicDownloader(QWidget):
         mainLayout.addWidget(nameLabel, 0, 0)
         mainLayout.addWidget(self.nameLine, 0, 1)
         mainLayout.addWidget(self.analysisButton, 0, 2)
-        mainLayout.addWidget(pathLineLabel, 1, 0, Qt.AlignTop)
+        mainLayout.addWidget(pathLineLabel, 1, 0)
         mainLayout.addWidget(self.pathLine, 1, 1)
         mainLayout.addWidget(self.browseButtun, 1, 2)
         mainLayout.addWidget(comicNameLabel, 2, 0)
@@ -41,7 +41,7 @@ class TencentComicDownloader(QWidget):
 
         self.setLayout(mainLayout)
         self.setWindowTitle("腾讯漫画下载")
-        self.setGeometry(0, 0, 800, 0)
+        self.setGeometry(400, 300, 800, 0)
 
 if __name__ == '__main__':
     import sys
